8bea2036ec02f3f571a1599edca4f3ced2c1c784
AminaSultana <101854377+AminaSultana@users.noreply.github.com>
2022-03-18 19:17:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+/* console.log(document.domain)
+console.log(document.title)
+console.log(document.URL)
+console.log(document.links)
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.forms[0])
+console.log(document.links)
+console.log(document.images)
+
+ */
+
+//getelementbyid
+//console.log(document.getElementById('items'))
+//let m = document.getElementById('header')
+/*console.log(m)
+m.textContent = 'hello'
+m.innerText = 'goodbye'
+console.log(m.innerText)
+m.innerHTML = '<h3>Hello</h3>'
+ */
+//m.style.borderBottom = 'solid 3px #000'
+
+let v = document.getElementById('additems')
+v.style.fontWeight = 'bold';
+v.style.color = 'green'
